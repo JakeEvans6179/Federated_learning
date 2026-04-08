f4fe793ee86b82526995fb8bfbbdce63faeae565
JakeEvans6179 <jakeyboyhk@icloud.com>
2026-04-08 01:23:26 +0100

    FL testing, tmr do chunking and test memory usage using different settings.

--- Init_test/gpu_test.py
+++ Init_test/gpu_test.py
@@ -1,3 +1,24 @@
+import os
+import ray
 import tensorflow as tf
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("Driver TF GPUs:", tf.config.list_physical_devices("GPU"))
+
+ray.shutdown()
+ray.init(ignore_reinit_error=True, num_cpus=16, num_gpus=1)
+
+print("Ray available resources:", ray.available_resources())
+
+@ray.remote(num_gpus=1)
+def gpu_probe():
+    import os
+    import ray
+    import tensorflow as tf
+    return {
+        "CUDA_VISIBLE_DEVICES": os.environ.get("CUDA_VISIBLE_DEVICES"),
+        "ray_gpu_ids": ray.get_gpu_ids(),
+        "tf_gpus": [d.name for d in tf.config.list_physical_devices("GPU")],
+    }
+
+print(ray.get(gpu_probe.remote()))
+ray.shutdown()
